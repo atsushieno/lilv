d722ed04d04e531e6cc0a57b64a4ad9d747056be
David Robillard <d@drobilla.net>
2019-04-01 00:14:47 +0200

    Remove Lilv::uri_to_path

--- lilv/lilvmm.hpp
+++ lilv/lilvmm.hpp
@@ -21,12 +21,6 @@
 
 namespace Lilv {
 
-LILV_DEPRECATED
-static inline const char*
-uri_to_path(const char* uri) {
-	return lilv_uri_to_path(uri);
-}
-
 #define LILV_WRAP0(RT, prefix, name) \
 	inline RT name() { return lilv_ ## prefix ## _ ## name (me); }
 
