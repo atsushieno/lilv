b16b753e76d7d1f336e0c717aa56594cbc51e4b2
Atsushi Eno <atsushieno@gmail.com>
2019-06-11 22:58:47 +0900

    expose abstract_set_io_context as public.

--- src/abstract_io.h
+++ src/abstract_io.h
@@ -1,9 +1,14 @@
 
+#ifndef __ABSTRACT_H_INCLUDED__
+#define __ABSTRACT_H_INCLUDED__
+
 #include <stdio.h>
 #include <stdarg.h>
 #include <sys/types.h>
+#include <lilv/lilv.h>
 
 /* nothing for desktop, AAssetManager* for Android */
+LILV_API
 void abstract_set_io_context (void* ioContext);
 
 /* lilv-specific */
@@ -14,3 +19,5 @@ int abstract_fseek(void* stream, long offset, int origin);
 void abstract_dir_for_each(const char* path,
                   void*       data,
                   void (*f)(const char* path, const char* name, void* data));
+
+#endif
